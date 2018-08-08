bc683e12c1e506911e287146838e504d29290356
iranvital <iranprogramador@gmail.com>
2018-08-07 23:40:17 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/iranvital/cursomc/CursomcApplication.java
+++ src/main/java/com/iranvital/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.iranvital.cursomc.domain.Cidade;
 import com.iranvital.cursomc.domain.Cliente;
 import com.iranvital.cursomc.domain.Endereco;
 import com.iranvital.cursomc.domain.Estado;
+import com.iranvital.cursomc.domain.ItemPedido;
 import com.iranvital.cursomc.domain.Pagamento;
 import com.iranvital.cursomc.domain.PagamentoComBoleto;
 import com.iranvital.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.iranvital.cursomc.repositories.CidadeRepository;
 import com.iranvital.cursomc.repositories.ClienteRepository;
 import com.iranvital.cursomc.repositories.EnderecoRepository;
 import com.iranvital.cursomc.repositories.EstadoRepository;
+import com.iranvital.cursomc.repositories.ItemPedidoRepository;
 import com.iranvital.cursomc.repositories.PagamentoRepository;
 import com.iranvital.cursomc.repositories.PedidoRepository;
 import com.iranvital.cursomc.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -122,6 +127,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p3, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 		
 		
 	}
--- src/main/java/com/iranvital/cursomc/domain/Pagamento.java
+++ src/main/java/com/iranvital/cursomc/domain/Pagamento.java
@@ -14,7 +14,7 @@ import com.iranvital.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 @Inheritance(strategy=InheritanceType.JOINED)
-public abstract class Pagamento  implements Serializable {
+public abstract class Pagamento implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
