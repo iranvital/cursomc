2b0ab7c06c9bbc510ec6f0b36b6fbfe2e1deee32
iranvital <iranprogramador@gmail.com>
2018-08-08 10:40:49 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/iranvital/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.iranvital.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iranvital.cursomc.domain.Pedido;
+import com.iranvital.cursomc.repositories.PedidoRepository;
+import com.iranvital.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException(
+				"Objeto nao encontrado! id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
