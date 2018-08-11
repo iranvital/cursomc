9ecb71596c6f4a6bca5ce1f4513e7fd1360197fc
iranvital <iranprogramador@gmail.com>
2018-08-10 22:46:41 -0300

    Inserindo Pedido

--- src/main/java/com/iranvital/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/iranvital/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.iranvital.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.iranvital.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	
 	private static final long serialVersionUID = 1L;
