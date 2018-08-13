7bc5d1af20a8b021018f31433afef42e797a2c92
iranvital <iranprogramador@gmail.com>
2018-08-13 10:50:36 -0300

    Email HTML

--- src/main/java/com/iranvital/cursomc/services/EmailService.java
+++ src/main/java/com/iranvital/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.iranvital.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.iranvital.cursomc.domain.Pedido;
@@ -9,5 +11,12 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	//Versao html dos metodos
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
+	
+	
 	
 }
